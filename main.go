6701b298971f5d10831588015cea279a0c8b2991
Воробьев Евгений <vorobiev_ev@mail.ru>
2025-09-02 16:00:01 +0300

    first iteration

--- main.go
+++ main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"sync"
+	"time"
 )
 
 const (
@@ -9,32 +12,94 @@ const (
 	CHUNKS = 8
 )
 
+var mu sync.Mutex
+
 // generateRandomElements generates random elements.
 func generateRandomElements(size int) []int {
 	// ваш код здесь
+
+	if size <= 0 {
+		fmt.Printf("значение %d<=0, выходит за пределы допустимого размера массива\n", size)
+		return []int{}
+	}
+	s := make([]int, size)
+	rand.NewSource(time.Now().UnixNano())
+		for i := 0; i < size; i++ {
+		s[i] = rand.Intn(size + 1)
+	}
+	return s
 }
 
 // maximum returns the maximum number of elements.
 func maximum(data []int) int {
 	// ваш код здесь
+	if len(data) == 0 {
+		panic("Ошибка: пустой срез")
+	}
+max := data[0]
+	for i := range data {
+		if max < data[i] {
+			max = data[i]
+		}
+	}
+	return max
 }
 
 // maxChunks returns the maximum number of elements in a chunks.
 func maxChunks(data []int) int {
 	// ваш код здесь
+	array := make([]int, CHUNKS)
+	var delta = (len(data) / CHUNKS) + 1
+	if len(data) == 0 {
+		panic("Ошибка: пустой срез")
+	}
+	if CHUNKS <= 0 {
+		panic("Ошибка: количество потоков<=0")
+	}
+
+	var wg sync.WaitGroup
+
+	wg.Add(CHUNKS)
+	for i := 0; i < CHUNKS; i++ {
+		go func(i int) {
+			defer wg.Done()
+			start := delta * i
+			stop := delta * (i + 1)
+			if i == CHUNKS-1 {
+				stop = len(data)
+			}
+			max := data[start]
+			for j := start; j < stop; j++ {
+				if max < data[j] {
+					max = data[j]
+				}
+			}
+			mu.Lock()
+			array[i] = max
+			mu.Unlock()
+		}(i)
+
+	}
+	wg.Wait()
+	return maximum(array)
 }
 
 func main() {
-	fmt.Printf("Генерируем %d целых чисел", SIZE)
+	fmt.Printf("Генерируем %d целых чисел\n", SIZE)
 	// ваш код здесь
-
+	arrSIZE := generateRandomElements(SIZE)
 	fmt.Println("Ищем максимальное значение в один поток")
+	start1 := time.Now()
+	max1 := maximum(arrSIZE)
+	elapsed := time.Since(start1)
+	elapsed1 := elapsed.Microseconds()
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max1, elapsed1)
+
+	fmt.Printf("Ищем максимальное значение в количестве потоков равном %d\n", CHUNKS)
 	// ваш код здесь
-
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
-
-	fmt.Printf("Ищем максимальное значение в %d потоков", CHUNKS)
-	// ваш код здесь
-
-	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d ms\n", max, elapsed)
+	start2 := time.Now()
+	max2 := maxChunks(arrSIZE)
+	elapsed = time.Since(start2)
+	elapsed2 := elapsed.Microseconds()
+	fmt.Printf("Максимальное значение элемента: %d\nВремя поиска: %d µs\n", max2, elapsed2)
 }
--- main_test.go
+++ main_test.go
@@ -1,3 +1,67 @@
 package main
 
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
 // Пишите тесты в этом файле
+func TestGenerateRandomElements(t *testing.T){
+	testingDataNegativ:=[]int{-100_000_000,-1000,-9,-8,-7,-1,0}
+	for _,j:=range testingDataNegativ {
+		result := generateRandomElements(j)
+		require.Len(t,result,0, "при тесте были заданы нулевые или отрицательные значения массива, тест не пройден")
+	}
+testingDataPozitiv:=[]int{1,7,8,9,100,300,500,1000,5000,20000,100000000}
+
+for _,j:=range testingDataPozitiv {
+result := generateRandomElements(j)	
+		require.Len(t,result,j,fmt.Sprintf("Ожидаемая длина: %d, фактическая: %d", j,result))
+	}
+// расчет среднего значения 
+for _,j:=range testingDataPozitiv {
+	sum:=0
+
+for _, value := range generateRandomElements(j){
+	sum +=value
+	}	
+	average:=float64(sum)/float64(j) 
+
+if average>float64(j)||average<0 {
+        t.Errorf("ожидаемое значение вне пределов допустимых значений  минимум %d максимум %d, получено %.2f", 0, j, average)
+    }	
+if j>500 {	
+minValue := average*0.9
+maxValue := average*1.1
+    if average < minValue || average > maxValue {
+        t.Errorf("ожидаемое значение вне пределов ожидаемого распределения от%.2f до%.2f, получено %.2f", minValue, maxValue, average)
+    }	}}}
+	func TestMaximum(t *testing.T){
+data:=[]int{0,1,2,3,4,5,6,7,8,9,10,100,200,300,500,1000,10000,100000,100000000}
+require.Equal(t,maximum(data),100000000,"тест с положительными числами не пройден")
+dataNegative:=[]int{-1,-2,-3,-4,-5,-6,-7,-8,-9,-10,-100,-200,-300,-500,-1000,-10000,-100000,-100000000}
+require.Equal(t,maximum(dataNegative),-1,"тест с отрицательными числами не пройден")
+dataMix:=[]int{0,-1,2,-3,4,-5,6,-7,8,-9,10,100,-200,300,-500,1000,-10000,100000,-100000000}
+require.Equal(t,maximum(dataMix),100000,"тест со смешанными числами не пройден")
+dataEqual:=[]int{20,20,20,20,20,20,20}
+require.Equal(t,maximum(dataEqual),20,"тест с одинаковыми числами не пройден")
+datasingl:=[]int{222}
+require.Equal(t,maximum(datasingl),222,"тест с одним числом не пройден")
+//тест на панику при пустом срезе
+       defer func() {
+        r := recover()
+			require.Equal(t, "Ошибка: пустой срез", r, "тест с пустым срезом не пройден")
+            require.NotNil(t, r, "Ожидалась паника при пустом срезе")
+        }()
+        maximum([]int{})    
+	}          
+   func TestComparisonMax(t *testing.T) {
+    // Генерируем тестовый массив
+    testData := generateRandomElements(100000000)
+    max1 := maximum(testData)
+    max2 := maxChunks(testData)
+    require.Equal(t, max1, max2, "Результаты поиска максимума отличаются. Ошибка")
+}    
+        
